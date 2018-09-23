6fb6fa7700f1a35a33106da2d4a1a847f5d940b5
Cotory <cotory@hainu.edu.cn>
2018-09-23 13:49:46 +0800

    update

--- Java/CS61b/proj1a/LinkedListDeque.java
+++ Java/CS61b/proj1a/LinkedListDeque.java
@@ -1,3 +1,4 @@
+
 /**
  * The generic placeholder must be the top of the class after the class name
  * And the generic placeholder can use the null to assign
@@ -9,36 +10,52 @@ public class LinkedListDeque<T> {
         public Node pre;
         public Node next;
         public T item;
-
-        public Node(Node p, Node n, T i) {
-            pre = p;
-            next = n;
-            item = i;
-        }
-        public Node() {
-            pre = null;
-            next = null;
-            item = null;
-        }
     }
     
     public Node sentinel_pre;
     public Node sentinel_next;
+    private int size;
 
     public LinkedListDeque() {
-        sentinel_pre = new Node();
-        sentinel_next = new Node();
+        sentinel_pre = new Node(null,null,null);
+        sentinel_next = new Node(null,null,null);
         sentinel_pre.next = sentinel_next;
         sentinel_next.pre = sentinel_pre;
+        size = 0;
+        
     }
         
     
     public LinkedListDeque(T x) {
-        sentinel_next.pre = sentinel_pre;
-        sentinel_pre.next = new Node(sentinel_pre, sentinel_pre.next, x);
-        
-
+        sentinel_pre = new Node(null, null, null);
+        sentinel_next = new Node(null, null, null);
+        sentinel_pre.next = new Node(sentinel_pre, sentinel_next, x);
+        sentinel_next.pre = sentinel_pre.next;
+        size += 1;
     }
+    
+    public void addFirst(T x) {
+        Node newNode = new Node(null, null, x);
+        sentinel_pre.next.pre = newNode;
+        newNode.next = sentinel_pre.next;
+        newNode.pre = sentinel_pre;
+        sentinel_pre.next = newNode;
+        size += 1;
+    }
+
+    public void addLast(T x) {
+        Node newNode = new Node(null, null, x);
+        sentinel_next.pre.next = newNode;
+        newNode.pre = sentinel_next.pre;
+        newNode.next = sentinel_next;
+        sentinel_next.pre = newNode;
+        size += 1;
+    }
+
+    public int size(){
+        return size;
+    }
+
     
   
 
